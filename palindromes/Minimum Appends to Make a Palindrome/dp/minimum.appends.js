54fe9816dc1505e321c162de630fcb62bc90fd4f
jonahkiprop34@gmail.com <jonahkiprop34@gmail.com>
2021-01-15 12:18:07 +0300

    #minimum appends- top-down approach

--- palindromes/Minimum Appends to Make a Palindrome/dp/minimum.appends.js
+++ palindromes/Minimum Appends to Make a Palindrome/dp/minimum.appends.js
@@ -1,30 +1,39 @@
 let dp = [];
-function computeDP(string){
-    dp = Array(string.length).fill(-1).map(row => Array(string.length).fill(-1));
-    console.log(dp);
-    for(let i = 0; i < string.length; i++) {
-        dp[i][i] = 1;
-    }
-    for(let substringlength = 2; substringlength <=string.length; substringlength++) {
-        for(let i = 0; i <=(string.length-substringlength); i++) {
-            let j = (substringlength+i)-1;
-            if(i === j-1 && string[i] === string[j]) {
-                console.log(j); console.log(string.length-1);
-                if(j == string.length-1) {
-                    dp[i][j] = 2;
-                } else
-                    dp[i][j] = 1;
-            } else if(string[i] === string[j]) {
-                dp[i][j] = 2 + Math.max(dp[i][j-1], dp[i+1][j]);
-            } else {
-                dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1]);
-            }
+function backtrack(string) {
+    let i = 0, j = string.length-1, count = 0;
+    while(i<j) {
+        if(dp[i][j] === 0) {
+            i++;j--;
+        } else {
+            count = count + dp[i][j];
+            i++;
         }
     }
-    console.log(dp);
-    return (string.length)- dp[0][string.length-1];
+    return count;
 }
-function minimumAppends(string) {
-    return computeDP(string);
+function computeDP(string) {
+    let i = 0, j = string.length-1;
+    while(i <= j) {
+        if(i>j) {
+            dp[i][j] = 0;
+            break;
+        } else if(i==j){
+            dp[i][j] = 0;
+            break;
+        } else if(string[i] === string[j]) {
+            dp[i][j] = 0;
+            i++; j--;
+        } else {
+            dp[i][j] = 1;
+            i++;
+        }
+    }
 }
-minimumAppends('abede');
+function MinimumAppends(string) {
+    dp = Array(string.length).fill(0).map(row => Array(string.length).fill(-1));
+    computeDP(string);
+    let count = backtrack(dp);
+    console.log(count);
+    return count;
+}
+MinimumAppends('aabb');
